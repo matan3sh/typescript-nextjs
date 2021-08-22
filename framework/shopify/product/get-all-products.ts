e6d4a660b233cb4d424ab1cd4c0208d064b9bb0e
matan3sh <matan3sh@gmail.com>
2021-08-22 12:12:48 +0300

    Fetch product query

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,15 +1,22 @@
-const fetchApi = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos';
+import getAllProductsQuery from '../utils/queries/get-all-products';
+
+type FetchParams = {
+  query: string;
+};
+
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = 'http://localhost:4000/graphql';
   const res = await fetch(url, {
-    method: 'GET',
+    method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query }),
   });
   const data = await res.json();
   return { data };
 };
 
 const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi();
+  const products = await fetchApi({ query: getAllProductsQuery });
   return products.data;
 };
 
