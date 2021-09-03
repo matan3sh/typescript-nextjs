7cf159d0d9a571f99dd262c3cea63200dae507a3
Matan Shaviro <matan3sh@gmail.com>
2021-09-03 19:58:03 +0300

    Handle corner cases in config

--- framework/common/config.js
+++ framework/common/config.js
@@ -3,8 +3,23 @@ const fs = require('fs');
 const merge = require('deepmerge');
 const prettier = require('prettier');
 
+const ALLOWED_FW = ['shopify', 'bigcommerce', 'shopify_local'];
+const FALLBACK_FW = 'shopify';
+
 function withFrameworkConfig(defaultConfig = {}) {
-  const framework = defaultConfig?.framework.name;
+  let framework = defaultConfig?.framework?.name;
+
+  if (!framework)
+    throw new Error('The api framework is missing, please add valid provider!');
+
+  if (!ALLOWED_FW.includes(framework))
+    throw new Error(
+      `The api framework: ${framework} cannot be found, please use one of ${ALLOWED_FW.join(
+        ', '
+      )}`
+    );
+
+  if (framework === 'shopify_local') framework = FALLBACK_FW;
 
   const frameworkNextConfig = require(path.join(
     '../',
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withFrameworkConfig } = require('./framework/common/config');
 module.exports = withFrameworkConfig({
   reactStrictMode: true,
   framework: {
-    name: 'shopify',
+    name: 'shopify_local',
   },
   i18: {
     locales: ['en-US', 'es'],
