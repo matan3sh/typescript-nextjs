e685b9ac69d02f86c7f1e5826025d7999bae1278
Matan Shaviro <matan3sh@gmail.com>
2021-09-20 11:39:18 +0300

    Start Implementing Product Info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,12 +1,37 @@
 import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { Product } from '@common/types/product';
+
+const placeholderImage = '/product-image-placeholder.svg';
 
 interface Props {
   product: Product;
 }
 
 const ProductCard: FC<Props> = ({ product }) => {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/products/${product.slug}`}>
+      <a>
+        <div>
+          <h3>{product.name}</h3>
+          <span>14 $</span>
+        </div>
+
+        {product.images && (
+          <Image
+            alt={product.name.toString() ?? 'Product Image'}
+            src={placeholderImage}
+            height={540}
+            width={540}
+            quality='85'
+            layout='responsive'
+          />
+        )}
+      </a>
+    </Link>
+  );
 };
 
 export default ProductCard;
