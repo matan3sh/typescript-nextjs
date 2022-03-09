ffc0e47897acd985182e56b9df4eec96dd2ff8bc
Matan Shaviro <matan3sh@gmail.com>
2022-03-09 07:50:49 +0200

    Make the container component reusable

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -2,14 +2,11 @@ import { ReactNode, FC } from "react";
 
 interface Props {
   children: ReactNode | ReactNode[];
+  el?: any;
 }
 
-const Container: FC<Props> = ({ children }) => {
-  return (
-    <div style={{ maxWidth: "1920px" }} className="px-6 mx-auto">
-      {children}
-    </div>
-  );
+const Container: FC<Props> = ({ children, el: Component = "div" }) => {
+  return <Component className="px-6 mx-auto max-w-8xl">{children}</Component>;
 };
 
 export default Container;
